bc98c04d9e1222d520e307c0815738390bb5140d
rafaelfrz <rafaelferrazswd2@protonmail.com>
2023-11-02 15:31:46 -0300

    i'm back :D, adding dashboard and some new keybindings and default org configs

---
+++ elpa/doom-modeline-20231028.1505/doom-modeline-pkg.el
@@ -0,0 +1,17 @@
+(define-package "doom-modeline" "20231028.1505" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "29.1.4.2")
+    (nerd-icons "0.1.0")
+    (shrink-path "0.3.1"))
+  :commit "dc15e932d8be149c9417a00898db229d9a2f8e6f" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
